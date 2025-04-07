cbeb5ee1fc13b09c94af8ba153f715e8a3bedb40
ItsWinterBush <154499206+ItsWinterBush26@users.noreply.github.com>
2025-04-08 06:21:41 +0800

    Load MobileGlues Libraries with gl4es fall back

--- app_pojavlauncher/src/main/java/net/kdt/pojavlaunch/utils/JREUtils.java
+++ app_pojavlauncher/src/main/java/net/kdt/pojavlaunch/utils/JREUtils.java
@@ -488,8 +488,11 @@ public class JREUtils {
                 renderLibrary = "libgl4es_114.so";
                 break;
         }
-
-        if (!dlopen(renderLibrary) && !dlopen(findInLdLibPath(renderLibrary))) {
+        
+        if (LOCAL_RENDERER.equals("opengles3_mges")) {
+             dlopen(NATIVE_LIB_DIR + "/libspirv-cross-c-shared.so");
+             dlopen(NATIVE_LIB_DIR + "/libshaderconv.so");
+        } else if (!dlopen(renderLibrary) && !dlopen(findInLdLibPath(renderLibrary))) {
             Log.e("RENDER_LIBRARY","Failed to load renderer " + renderLibrary + ". Falling back to GL4ES 1.1.4");
             LOCAL_RENDERER = "opengles2";
             renderLibrary = "libgl4es_114.so";
