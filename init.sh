1cac5dde280414d81b7594549d6390ccef8a083b
alexytomi <60690056+alexytomi@users.noreply.github.com>
2025-04-13 02:29:12 +0800

    make shell script to initialize the worktree automatically

--- init.sh
+++ init.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
 
-for dir in *; do
-  if [ -d "$dir" ]; then
-    command="git worktree add "$dir" origin/feat/"$dir""
-    echo ">> $command"
-    $command
-  fi
-done
+if [ "$1" = "force" ]; then
+  for dir in *; do
+    if [ -d "$dir" ]; then
+      command="git worktree add -f "$dir" origin/feat/"$dir""
+      echo ">> $command"
+      $command
+    fi
+  done
+else
+  for dir in *; do
+    if [ -d "$dir" ]; then
+      command="git worktree add "$dir" origin/feat/"$dir""
+      echo ">> $command"
+      $command
+    fi
+  done
+fi
