9603bd38677726e3b9db468620ff3fb76733c985
alexytomi <60690056+alexytomi@users.noreply.github.com>
2025-05-30 14:25:41 +0800

    test2

--- app_pojavlauncher/src/main/java/net/kdt/pojavlaunch/services/GameService.java
+++ app_pojavlauncher/src/main/java/net/kdt/pojavlaunch/services/GameService.java
@@ -49,7 +49,7 @@ public class GameService extends Service {
                 .setContentText(getString(R.string.notification_game_runs))
                 .setContentIntent(contentIntent)
                 .addAction(android.R.drawable.ic_menu_close_clear_cancel,  getString(R.string.notification_terminate), pendingKillIntent)
-                .setSmallIcon(R.drawable.notif_icon)
+                .setSmallIcon(R.mipmap.ic_launcher)
                 .setNotificationSilent();
 
         Notification notification = notificationBuilder.build();
